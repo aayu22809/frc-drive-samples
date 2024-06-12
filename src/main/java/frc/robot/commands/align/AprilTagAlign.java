f0dc66cccd081607b99b6c4d724582d7a3b6b62a
Bhargav Eranki <77950550+beranki@users.noreply.github.com>
2024-06-11 18:04:48 -0700

    at changes

--- src/main/java/frc/robot/commands/align/AprilTagAlign.java
+++ src/main/java/frc/robot/commands/align/AprilTagAlign.java
@@ -1,45 +1,63 @@
-package frc.robot.commands.align;
+package frc.robot.commands.mech;
 
-//import org.photonvision.PhotonCamera;
-
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.HardwareMap;
+import frc.robot.MechConstants;
+
+// Third party Hardware Imports
+import com.revrobotics.CANSparkMax;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.Encoder;
+import frc.robot.systems.DriveFSMSystem;
 
 public class AprilTagAlign extends Command {
-
-	private final PhotonLL photonCamera;
+	private int atID;
 
 	/**
-	 * Creates a new ExampleCommand.
-	 *
-	 * @param subsystem The subsystem used by this command.
+	 * Makes a command that aligns robot to pose using DriveFSMSystem.
+	 * @param id id of AT to align with
 	 */
-	public PhotonLLCommand() {
-		photonCamera = PhotonLL.getInstance();
-
-		addRequirements(photonCamera);
+	public AprilTagAlign(int id) {
+		atID = id;
 	}
 
 	// Called when the command is initially scheduled.
 	@Override
 	public void initialize() {
-
 	}
 
 	// Called every time the scheduler runs while the command is scheduled.
 	@Override
 	public void execute() {
-
+		intakeMotor.set(MechConstants.INTAKE_POWER);
 	}
 
 	// Called once the command ends or is interrupted.
 	@Override
 	public void end(boolean interrupted) {
-
+		intakeMotor.set(0);
 	}
 
 	// Returns true when the command should end.
 	@Override
 	public boolean isFinished() {
 		return false;
+	}
+
+	private double pidAuto(double currentEncoderPID, double targetEncoder) {
+		double correction = MechConstants.PID_CONSTANT_PIVOT_P_AUTO
+			* (targetEncoder - currentEncoderPID);
+
+		return Math.min(MechConstants.MAX_TURN_SPEED_AUTO,
+			Math.max(MechConstants.MIN_TURN_SPEED_AUTO, correction));
+	}
+
+	private boolean inRange(double a, double b) {
+		return Math.abs(a - b) < MechConstants.INRANGE_VALUE; //EXPERIMENTAL
 	}
 }
